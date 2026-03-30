ab7a839236189783512b53519f257f7cb7ac1ef0
jonfreedman <jon.freedman@zoho.com>
2026-03-30 11:26:38 +0100

    Fix incorrect `-branch` flag to `--branch` in bead launchers

--- scripts/launch_bead_codex.sh
+++ scripts/launch_bead_codex.sh
@@ -117,7 +117,7 @@ if git show-ref --verify --quiet "refs/heads/$TASK_BRANCH"; then
   fail "Branch already exists: $TASK_BRANCH"
 fi
 
-bd worktree create "$WORKTREE_REL" -branch "$TASK_BRANCH"
+bd worktree create "$WORKTREE_REL" --branch "$TASK_BRANCH"
 
 echo "Claimed bead: $CLAIMED_ID"
 echo "Base branch: $BASE_BRANCH"
